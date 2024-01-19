967f3a29279090774bf8182f8bee9ac9c4256226
Janne Huotari <janne.huotari@teamit.fi>
2024-01-19 17:24:01 +0200

    Add very necessary loader animation

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 const FORM = document.querySelector('#question-form');
 const ANSWER = document.querySelector('#answer');
 const API_URL = 'http://localhost:3000/question';
+const LOADER = document.querySelector('#loader');
 
 FORM.onsubmit = async (e) => {
   e.preventDefault();
+  LOADER.style.display = 'block';
   const question = FORM.elements.question.value;
   const response = await fetch(API_URL, {
     method: 'POST',
@@ -14,4 +16,5 @@ FORM.onsubmit = async (e) => {
   });
   const answer = await response.json();
   ANSWER.innerHTML = answer.text;
+  LOADER.style.display = 'none';
 };
