8d286904a4d3051525b89a0e16134462916b1662
Janne Huotari <janne.huotari@teamit.fi>
2024-01-19 14:37:19 +0200

    Add readme. Add simple test to get answer from pdf file

--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 # POC testing for AI document reader
+
+1. add .env file and add you API key there
+2. source .env
+3. run `node single-doc.js`
--- single-doc.js
+++ single-doc.js
@@ -1,14 +1,35 @@
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { OpenAI } from '@langchain/openai';
+import { loadQAStuffChain } from 'langchain/chains';
+
+const OPEN_AI_API_KEY = process.env.OPEN_AI_API_KEY;
+
+const model = new OpenAI({
+  modelName: 'gpt-3.5-turbo-instruct', // Defaults to "gpt-3.5-turbo-instruct" if no model provided.
+  temperature: 0.9,
+  openAIApiKey: OPEN_AI_API_KEY // In Node.js defaults to process.env.OPENAI_API_KEY
+});
 
 const loadPDF = async () => {
   const loader = new PDFLoader('./docs/RachelGreenCV.pdf');
 
   const docs = await loader.load();
-  console.log(docs);
+  return docs;
 };
 
-const main = () => {
-  loadPDF();
+const chat = async (docs) => {
+  const chain = await loadQAStuffChain(model);
+  const question = 'Who is the CV about?';
+  const response = await chain.invoke({
+    input_documents: docs,
+    question
+  });
+  console.log(response);
+};
+
+const main = async () => {
+  const docs = await loadPDF();
+  chat(docs);
 };
 
 main();
