e56d95256e2b9ffc9e78c49ab25fd7f1adb1fd3c
Janne Huotari <janne.huotari@teamit.fi>
2024-01-19 17:33:57 +0200

    Add error handling to ui

--- src/main.js
+++ src/main.js
@@ -2,19 +2,26 @@ const FORM = document.querySelector('#question-form');
 const ANSWER = document.querySelector('#answer');
 const API_URL = 'http://localhost:3000/question';
 const LOADER = document.querySelector('#loader');
+const ERROR = document.querySelector('#error');
 
 FORM.onsubmit = async (e) => {
   e.preventDefault();
   LOADER.style.display = 'block';
   const question = FORM.elements.question.value;
-  const response = await fetch(API_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ question })
-  });
-  const answer = await response.json();
-  ANSWER.innerHTML = answer.text;
-  LOADER.style.display = 'none';
+  try {
+    const response = await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ question })
+    });
+    const answer = await response.json();
+    ANSWER.innerHTML = answer.text;
+    LOADER.style.display = 'none';
+  } catch (error) {
+    LOADER.style.display = 'none';
+    ERROR.style.display = 'block';
+    ANSWER.innerHTML = 'error!!';
+  }
 };
